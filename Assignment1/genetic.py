72d0e0a5fe105e6d6f2b738105374665326b87db
William Dahl <williada@ulrik.uio.no>
2018-09-16 18:06:57 +0200

    children mutation done

--- Assignment1/genetic.py
+++ Assignment1/genetic.py
@@ -48,36 +48,55 @@ def parent_selection(population, distances, selection_size):
     return parent_one, parent_two
 
 
+def mutate_child(child):
+    start = random.randint(len(child))
+    stop = random.randint(len(child))
+    print(f'{start}:{stop}')
+
+    return child
+
 def pmx(p1, p2,start,stop):
+
+    '''
+    Yes, I am so lazy that i convert from list to np.array on parents,
+    and from np.array to list in children.
+    '''
     child = [None]*len(p1) #init a empty list
     child[start:stop] = p1[start:stop] # copy a silce of parent
-
+    parent_one = p1.tolist()
+    parent_two = p2.tolist()
+    #print(p1)
+    #print(p2)
     # map the slice from p2 into child using the indices from p1
-    print(child)
-    for i,j in enumerate(p2[start:stop]):
+    #print(child)
+    #print("hello")
+    for i,j in enumerate(parent_two[start:stop]):
         i += start
         # now we check if there are genes in p2 substring that are not in the child yet.
         if j not in child:
         #    print('this is j '+str(j))
             while child[i] != None:
         #        print('this is i: ' + str(i))
-                i = p2.index(p1[i])
+                i = parent_two.index(parent_one[i])
+                #i = np.where(p2 == p1[i])
         #        print('this is  new i: ' + str(i))
             child[i] = j
 
-    print(child)
     # copy the rest from parent2
     for i,j in enumerate(child):
         if j == None:
             child[i] = p2[i]
 
+#    print(child)
+    child = np.array(child)
     return child
 
 
 def pair_pmx(p1,p2):
     half = len(p1) // 2 #rounded down
-    start = rand.randint(0, len(p1)-half)
+    start = randint(0, len(p1)-half)
     stop = half + start #now we only choose half of the genomes of the parent
+    print(f'pair_pmx: {start} : {stop}')
     return pmx(p1,p2,start,stop), pmx(p2,p1,start,stop) # call to pmx and create the crossover children.
 
 
@@ -111,6 +130,7 @@ def calculate_distance(p,distances):
     return sum
 
 
+
 if __name__ == '__main__':
         cities, distances = reader("european_cities.csv")
         # 10 cities
@@ -122,5 +142,9 @@ if __name__ == '__main__':
 
         population = make_population(population_size,N)
         #print(population)
-        p1,p2 = parent_selection(population, distances, selection_size)
-        
+        parent_one, parent_two = parent_selection(population, distances, selection_size)
+        print(parent_one)
+        print(parent_two)
+        child_one, child_two = pair_pmx(parent_one, parent_two)
+        print(child_one)
+        print(child_two)
