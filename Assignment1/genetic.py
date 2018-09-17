417b2f485dc472774db942f65c5c8041e8f616f6
William Dahl <williada@ulrik.uio.no>
2018-09-18 00:22:31 +0200

    I might have done something very wrong with the calculations, need to fix that. Might need to take the worst of the best and not the worst of the worst

--- Assignment1/genetic.py
+++ Assignment1/genetic.py
@@ -3,9 +3,10 @@
 Code based on solutions for week 2 tasks. Thank you TA, you are the MVP
 '''
 import csv
-from random import shuffle , sample, randint
+from random import shuffle , sample, randint, random
 import time
 import numpy as np
+from sys import exit
 
 
 def make_population(size,N):
@@ -22,18 +23,22 @@ def make_population(size,N):
 
 
 def parent_selection(population, distances, selection_size):
-
+    #print('in parent selections')
+    #print(population)
+    #print(distances)
+    #print(selection_size)
     random_selections = sample(population,selection_size)
     selection_distances = []
 
     for i in range(len(random_selections)):
-        calc = calculate_distance(random_selections[i],distances)
+        #calc = calculate_distance(random_selections[i],distances)
         #print(f'This is the {i} parent, total distance is : {calc}')
         selection_distances.append(calculate_distance(random_selections[i], distances))
 
 
     #print(f'random_selections before pop : {random_selections}')
     shortest_route_index = np.argmin(selection_distances)
+    #print(f'parent_selection {shortest_route_index}')
     #print(f'shortest route index : {shortest_route_index}')
     parent_one = random_selections[shortest_route_index]
     #print(parent_one)
@@ -44,20 +49,24 @@ def parent_selection(population, distances, selection_size):
     shortest_route_index = np.argmin(selection_distances)
     parent_two = random_selections[shortest_route_index]
     #print(parent_two)
-
+    #print('here are the parents')
+    #print(parent_one)
+    #print(parent_two)
     return parent_one, parent_two
 
 
-def mutate_child(child):
-    half = len(child) // 2 #rounded down
-    start = randint(0, len(child)-half)
+def mutate_citizen(citizen):
+    #print('before')
+    half = len(citizen) // 2 #rounded down
+    #print('hello')
+    start = randint(0, len(citizen)-half)
     stop = half + start
     #print(f'{start}:{stop}')
     #print(f'child before: {child}')
-    child[start:stop] = child[start:stop][::-1]
+    citizen[start:stop] = citizen[start:stop][::-1]
     #print(f'child after: {child}')
 
-    return child
+    return citizen
 
 def pmx(p1, p2,start,stop):
 
@@ -69,21 +78,12 @@ def pmx(p1, p2,start,stop):
     child[start:stop] = p1[start:stop] # copy a silce of parent
     parent_one = p1.tolist()
     parent_two = p2.tolist()
-    #print(p1)
-    #print(p2)
-    # map the slice from p2 into child using the indices from p1
-    #print(child)
-    #print("hello")
+
     for i,j in enumerate(parent_two[start:stop]):
         i += start
-        # now we check if there are genes in p2 substring that are not in the child yet.
         if j not in child:
-        #    print('this is j '+str(j))
             while child[i] != None:
-        #        print('this is i: ' + str(i))
                 i = parent_two.index(parent_one[i])
-                #i = np.where(p2 == p1[i])
-        #        print('this is  new i: ' + str(i))
             child[i] = j
 
     # copy the rest from parent2
@@ -91,7 +91,7 @@ def pmx(p1, p2,start,stop):
         if j == None:
             child[i] = p2[i]
 
-#    print(child)
+
     child = np.array(child)
     return child
 
@@ -100,7 +100,7 @@ def pair_pmx(p1,p2):
     half = len(p1) // 2 #rounded down
     start = randint(0, len(p1)-half)
     stop = half + start #now we only choose half of the genomes of the parent
-    print(f'pair_pmx: {start} : {stop}')
+    #print(f'pair_pmx: {start} : {stop}')
     return pmx(p1,p2,start,stop), pmx(p2,p1,start,stop) # call to pmx and create the crossover children.
 
 
@@ -124,6 +124,93 @@ def reader(filename):
         city_line += 1
     return cities, distances
 
+def genetic(cities, distances, population_size, max_generations, selection_size, mutation_rate,num_cities):
+
+    population = make_population(population_size, num_cities)
+    elites = []
+    average_fitness = []
+    generation = 0
+    searched = 0
+    #print(population)
+
+#    print('in genetic')
+    while generation < max_generations:
+    #    print(f'{generation} vs {max_generations}')
+        lenght = len(population)
+    #    print(f'This is the population: {population}\n Size is : {lenght}')
+
+        generation_distances = []
+        #generation_distances =[calculate_distance(route,distances) for route in population]
+    #    print(f'this is RIGHT before appending : {generation_distances}')
+
+        for i in range(population_size):
+            generation_distances.append(calculate_distance(population[i], distances))
+
+    #    print(f'this is the generation_distances: {generation_distances}')
+        shortest_route_index = np.argmin(generation_distances)
+        shortest_route = generation_distances[shortest_route_index]
+    #    print(f'here is the shortest route index so far : {shortest_route_index}. Value is : {shortest_route}')
+        new_population = [population[shortest_route_index]] # save the best form previous gen
+
+        elites.append(generation_distances[shortest_route_index]) # save for calculating avg/best/worst
+    #    print(f'current population: {new_population}')
+        #saving ~5% of the previous generation for next just to get some more randomnes
+
+        for i in range(population_size//20):
+            lucky_citizen = population[randint(0,population_size-1)]
+            new_population.append(lucky_citizen)
+
+    #    print(f'current population AFTER lucky_citizen: {new_population}')
+
+        while len(new_population) < population_size:
+            #print(distances)
+            parent_one, parent_two = parent_selection(population, distances, selection_size)
+            child_one, child_two = pair_pmx(parent_one, parent_two)
+            #print(f'here is child_one : {child_one}')
+            #print(f'here is child_two : {child_two}')
+
+            new_population.append(child_one)
+            new_population.append(child_two)
+
+        #    tmp = len(new_population)
+        #    print(f'size of new pop {tmp}')
+
+        #print('population is created')
+
+        if (max_generations/generations) > 0.8:
+            mutation_rate = 0.025 # decrease mutation rate as we come within the last 20% runs
+            for i in range(population_size):
+                if random() < mutation_rate:
+                    new_population[i] = mutate_citizen(new_population[i])
+
+
+    #    print(f' ELITES   {elites}')
+
+        average_fitness.append(np.mean(elites)) # save mean of elites to fitness as required
+        #print(f'here is the avarage fitness so far: {average_fitness}')
+        searched += len(population)
+        population = new_population
+        generation += 1
+    #    print('CAME THROUGH WHOLE WHILE')
+
+
+
+    #print('DONEEEEE\n\n')
+
+
+
+    population_distances = []
+
+    for i in range(population_size):
+        population_distances.append(calculate_distance(population[i],distances))
+
+    #print(f' This is final population: {population}')
+    #print(f' This is the average_fitness_array: {average_fitness}')
+    #print(f' This is population_distances: {population_distances}')
+    #print(f' This is the elites: {elites}')
+
+
+    return population, population_distances, average_fitness, searched
 def calculate_distance(p,distances):
 
     sum = 0
@@ -134,22 +221,83 @@ def calculate_distance(p,distances):
     return sum
 
 
+def genetic_start(cities, distances, population_size, max_generations, num_cities,runs):
+
+        mutation_rate = 0.05 # starting with this and changing
+        selection_size = population_size//10 # give also weeker individuals a chance to make babies
+
+        best_routes_all_run = []
+        best_distance_all_run = []
+
+        worst_distance_all_run = []
+
+        average_distance_all_run = []
+        std_all_runs = []
+
+        planet_fit = []
+        total_searched = 0
+        start_time = time.time()
+
+        for i in range(runs):
+
+            p, d, f, s = genetic(cities, distances, population_size, max_generations, selection_size, mutation_rate, num_cities)
+
+            best_route_index = np.argmin(d)   # best rounte index
+            best_routes_all_run.append(p[best_route_index])
+            best_distance_all_run.append(d[best_route_index]) # best distance
+
+
+            worst_route_index = np.argmax(d)
+            worst_distance_all_run.append(max(d)) # longest distance
+
+
+            average_route_length = np.sum(d)/len(d)
+            average_distance_all_run.append(average_route_length)
+
+            std_all_runs.append(np.std(d))
+            total_searched += s
+
+
+        end_time = time.time() - start_time
+        glb_best_route_index = np.argmin(best_distance_all_run) # get the best route
+        global_best_dist = min(best_distance_all_run) # get the best distance
+        #global_best_dist = best_distance_all_run[glb_best_route_index]
+        #glb_worst_route_index = np.argmax(best_distance_all_run)
+
+        global_worst_dist = max(worst_distance_all_run) # the worst distance of all
+
+
+        global_average_dist = np.sum(average_distance_all_run)/len(average_distance_all_run)
+        average_std = np.sum(std_all_runs)/len(std_all_runs)
+        phenotype = geno_to_pheno(best_routes_all_run[best_route_index],cities)
+
+
+        print(f'{num_cities} cities, with {population_size} population and {max_generations} generations:\n Length best tour: {global_best_dist:2.2f}.\n Length of worst tour: {global_worst_dist:2.2f}.\n Length of average tour: {global_average_dist:2.2f}.\n Standard diviation is: {average_std}.\n Route of the best tour is: {phenotype}\n Searched {total_searched} routes.\n Runtime: {end_time:2.4f}\n\n')
+
+def geno_to_pheno(genotype, cities):
+    pheno = []
+    for i in genotype:
+        pheno.append(cities[i])
+    return pheno
+
 
 if __name__ == '__main__':
         cities, distances = reader("european_cities.csv")
         # 10 cities
-
-        N = 10 # 10 cities
+        num_cities = 10
+        runs = 20
+        all_stars_fitness = []
+        population_size= [ 50, 100, 150]
         generations = 50
-        population_size = 20
-        selection_size = population_size//4
 
-        population = make_population(population_size,N)
+
+
+
+        for pop in population_size:
+            genetic_start(cities, distances, pop, generations, num_cities,runs)
+
+        N_cities = 24
+        population_size= [100, 250, 600]
+        generations = 100
+
         #print(population)
-        parent_one, parent_two = parent_selection(population, distances, selection_size)
-        print(parent_one)
-        print(parent_two)
-        child_one, child_two = pair_pmx(parent_one, parent_two)
-        print(child_one)
-        print(child_two)
-        child_one, child_two = mutate_child(child_one), mutate_child(child_two)
