cfbec6b5b1b696c49b9fec9f61509953b0a339ed
William Dahl <williada@ulrik.uio.no>
2018-10-10 21:57:29 +0200

    backrop should be done, works in the test

--- Assignment2/code/mlp.py
+++ Assignment2/code/mlp.py
@@ -10,7 +10,7 @@ class mlp:
     def __init__(self, inputs, targets, nhidden):
         self.beta = 1
         self.bias = 1
-        self.eta = 0.1
+        self.eta = 0.1 # learning rate
         self.momentum = 0.0
         ninputs = len(inputs[0]) + 1 # one extra for our bias, which is not included in the dataset
         noutput = len(targets[0])
@@ -42,50 +42,116 @@ class mlp:
         # can do a test first and check vs error, if worse go train more. while true
         #hidden_output = train(self.hlw, inputs)
         #print(hidden_output)
-        final_hidden_output, final_output = self.forward(inputs)
+    #    final_hidden_output, final_output = self.forward(inputs)
+        test = self.train(inputs, targets,100)
         print(final_hidden_output)
         print(final_output)
 
 
         print('To be implemented')
 
-    def train(self, inputs, targets, iterations=100):
+    def train(self, inputs, targets, iterations):
         # need a counter to specify which input array we are working on
         print('To be implemented')
+        print('number of iterations:',iterations)
+        for it in range(iterations):
+            for i in range(len(inputs)):
+                #forward
+                hidden_out, final_output = self.forward(inputs[i])
+                print('In train hidden ', hidden_out)
+                print('In train output ', final_output)
+                #backward
+                self.back_prop(hidden_out,final_output,targets[i],inputs[i])
+
+# TODO: continue from this line
 
 
 
-    def forward(self, inputs):
+    def forward(self, vector):
+        print(vector)
         # inner layer output
         #print('FORWARD inputs: ', len(inputs))
         #print(inputs[0])
         ##print('this ex')
         #print(self.hlw[0])
     #    print('before: ', self.hlw[0])
-        for i in range(len(inputs)):
-            # create hidden_output from one training sample
-            hidden_out = self.matrix_hidden(inputs,i)
-            print('First round hidden:', hidden_out)
-            hidden_out = self.sigmoid(hidden_out)
-            print('After sigmoid hidden', hidden_out)
-            final_output = self.matrix_outer(hidden_out)
-            print('First round outer: ', final_output)
-            final_output = self.activation_outer(final_output)
+        # create hidden_output from one training sample
+        hidden_out = self.matrix_hidden(vector)
+        print('First round hidden:', hidden_out)
+        hidden_out = self.sigmoid(hidden_out)
+        print('After sigmoid hidden', hidden_out)
+        final_output = self.matrix_outer(hidden_out)
+        print('First round outer: ', final_output)
+        final_output = self.activation_outer(final_output)
         #    print('After activation outer:', final_output)
-            # now i will call back propagation on my Values
+            # now i will call back propagation on my Values to update the weight matrices
         #    print('after: ', self.hlw[0])
-            
-            exit(0)
 
 
-        print(hidden_out)
-        print('DONE')
+    #    print(hidden_out)
+    #    print('DONE')
         # send to sigmoid
         # outer layer output
-        final_output = self.matrix(hidden_out)
-        print(final_output)
+#        final_output = self.matrix(hidden_out)
+    #    print(final_output)
         # send to sigmoid
         return hidden_out, final_output
+
+    def back_prop(self, hidden_output, final_output,targets,vector):
+        print(targets)
+        eo = self.error_outer(final_output, targets) # delta outer
+        ei = self.error_hidden(hidden_output,eo,targets) # delta inner
+        #ready to update values for olw.
+        self.update_weights_outer(hidden_output, eo)
+        print(vector)
+        #print('before ',self.hlw[0])
+        self.update_weights_inner(vector,ei)
+    #    print('after ',self.hlw[0])
+
+        exit(0)
+        return(0)
+
+
+
+    def update_weights_inner(self,vector,ei):
+        print('nodes: ',len(ei))
+        print('inputlen',len(vector))
+        print('hlw', len(self.hlw[0]))
+
+        for i in range(len(self.hlw)):
+            for j in range(len(vector)):
+            #    print(j)
+            #    print(weights2[i])
+                self.hlw[i][j] = self.hlw[i][j] - (self.eta *ei[i] * vector[j])
+
+
+    # hidden_output is the activation from hidden
+    def update_weights_outer(self, hidden_output, eo):
+        for i in range(len(self.olw)):
+            for j in range(len(hidden_output)):
+                self.olw[i][j] = (self.olw[i][j]-hidden_output[j]*eo[i]*self.eta)
+
+
+    # 4.9 from the book
+    # calculate the delta for each neuron in hidden layer
+    def error_hidden(self, hidden_output, eo,targets):
+        ei = []
+        for i in range(len(hidden_output)):
+            answ = 0
+            # loop over all indexes downwards in the matrix
+            for j in range(len(targets)):
+                answ += self.olw[j][i]*eo[j]
+            ei.append(answ)
+
+        #ei = [hidden_out[i]*(1-hidden_out[i] * self.olw[i]*eo[i] for i in range(len(hidden_out))]
+        return ei
+
+    # 4.14 from the book
+    def error_outer(self, final_output, targets):
+        eo = [(final_output[i]-targets[i]) for i in range(len(final_output))] # these are the deltas
+        print('print eo:', eo)
+        return(eo)
+
 
     def confusion(self, inputs, targets):
         print('To be implemented')
@@ -101,7 +167,7 @@ class mlp:
         return weightedsum
 
     # helper funcion for calulating weightetsum for each neuron
-    def matrix_hidden(self, inputs,i):
+    def matrix_hidden(self, vector):
         weightedsum = []
     #    print('here is nhidden', self.nhidden)
     #    print('here is hlw[0][0]',self.hlw[0][0])
@@ -117,7 +183,7 @@ class mlp:
     #        print(inputs[0])
             for j in range(len(self.hlw[0])-1):
             #    print('j',j)
-                answ += self.hlw[i][j]*inputs[i][j]
+                answ += self.hlw[i][j]*vector[j]
                 answ += self.hlw[i][self.ninputs-1] * self.bias
             weightedsum.append(answ)
         #returns an array of the weighted sums.
--- Assignment2/test.py
+++ Assignment2/test.py
@@ -22,7 +22,6 @@ def matrix(inputs, weights,nhidden, number):
     weightedsum = []
     print(inputs[number][1])
     for x in range(2):
-
         for i in range(nhidden):
             answ = 0
             for j in range(len(weights[0])-1):
@@ -31,16 +30,73 @@ def matrix(inputs, weights,nhidden, number):
                 weightedsum.append(answ)
     return weightedsum
 
-if __name__ == '__main__':
-    inputs = [[1,2,3,4],[4,3,2,1]]
-    bias = 0.5
-    beta = 1
-    weights = [[1,1,1,1,1],[-2,-2,-2,-2,-2],[3,3,3,3,3]]
-    nhidden = 3
-    ninputs = 4
-    number = 0
 
-    weightedsum = matrix(inputs,weights,nhidden,number)
-    print(weightedsum)
-    new = sigmoid(weightedsum)
-    print(new)
+def error_test(delta_hidden, weights, errors):
+    ret = []
+    for i in range(len(hidden_errors)):
+        answ = 0
+        for j in range(len(errors)):
+            answ += weights[j][i] * errors[j]
+
+        ret.append(answ)
+
+    return ret
+
+def update_weights_outer(weights2,eta,hidden_activate,errors):
+
+    print(weights2)
+    print('weights', np.shape(weights2))
+    print('errors', np.shape(errors))
+    print('hidden activate', np.shape(hidden_activate))
+    #print(len(weights2))
+    for i in range(len(weights2)):
+
+    #    print(len(weights2[0]))
+        for j in range(len(hidden_activate)):
+        #    print(weights2[i])
+            #print('error',errors[i])
+            weights2[i][j] = (weights2[i][j] - (hidden_activate[j]*errors[i]*eta))
+
+        #    print(answ)
+
+    print(weights2)
+
+def update_inner(weights2, eta, inn, error):
+    print('inner')
+    print('weights: ',np.shape(weights2))
+    print('error: ',np.shape(error))
+    print('inn: ',np.shape(inn))
+
+    
+
+
+    for i in range(len(weights2)):
+        for j in range(len(inn)):
+        #    print(weights2[i])
+            weights2[i][j] = (weights2[i][j]-(eta*error[i]*inn[j]))
+
+    print(weights2)
+
+if __name__ == '__main__':
+    # inputs = [[1,2,3,4],[4,3,2,1]]
+    # bias = 0.5
+    # beta = 1
+    # weights = [[1,1,1,1,1],[-2,-2,-2,-2,-2],[3,3,3,3,3]]
+    # errors = [2,3]
+    # delta_hidden =[0,0,0]
+    # nhidden = 3
+    # ninputs = 4
+    # number = 0
+#    delta_hidden = error_test(delta_hidden, weights2,errors)
+
+
+    weights2 = [[1,2,3],[2,2,2]]
+    eta = 0.1
+    inn = [1,2,1]
+    error = [0.5, 1]
+    #new_weights = update_weights_outer(weights2, eta, hidden_activate,errors)
+    update_inner(weights2, eta, inn, error)
+#weightedsum = matrix(inputs,weights,nhidden,number)
+    #print(weightedsum)
+    #new = sigmoid(weightedsum)
+    #print(new)
